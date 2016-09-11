3c4073b0b273246d0594373aac802245c745e412
Ahmet Türk <ahmetturk93@gmail.com>
2016-09-11 13:09:09 +0300

    Initialize

---
+++ AutoNotifyPropertyChanged/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoNotifyPropertyChanged
+{
+    public class Class1
+    {
+    }
+}
